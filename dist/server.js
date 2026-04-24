67352f954df8a945b9377128ad2043ec6ec358c1
Sam-Teche <ogunrindesam@gmail.com>
2026-04-24 09:21:17 +0100

    add patch

--- dist/server.js
+++ dist/server.js
@@ -18,7 +18,7 @@ app.use((0, cors_1.default)({
         "http://localhost:3000",
         "http://localhost:3001",
     ],
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
 }));
--- src/server.ts
+++ src/server.ts
@@ -17,7 +17,7 @@ app.use(
       "http://localhost:3000",
       "http://localhost:3001",
     ],
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   }),
